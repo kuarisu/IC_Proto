b51e23ffe6baf3f3b859ce600a37bdb15e0d77d0
kuarisu <kuarisuran@gmail.com>
2016-12-20 12:17:41 +0100

    Add value multiplier

--- Assets/Scripts/Multiplier/Multiplier_Spawn.cs
+++ Assets/Scripts/Multiplier/Multiplier_Spawn.cs
@@ -4,19 +4,28 @@ using UnityEngine;
 
 public class Multiplier_Spawn : MonoBehaviour {
 
+    [SerializeField]
+    Animator m_An;
+
     Vector3 m_DirectionToPlayer;
     Vector3 m_PlayerPos;
     Vector3 m_PreviousPos;
+    bool m_CanBeTaken;
+
 
 	// Use this for initialization
 	void Start () {
+        m_CanBeTaken = false;
         m_PreviousPos = transform.position;
         m_PlayerPos = GameManager_PlayerPos.Instance.m_PlayerPosition.position;
+
         m_DirectionToPlayer = (m_PlayerPos - transform.position).normalized;
         transform.position = transform.position + new Vector3 ( Random.Range(-3, 3) + (m_DirectionToPlayer.x * 2), 0, Random.Range(-3, 3) + (m_DirectionToPlayer.z * 2)) ;
         transform.position = new Vector3(transform.position.x, 0.25f, transform.position.z);
 
         ChangeRotation();
+        StartCoroutine(EnableColliderAndAnim());
+
 	}
 
     void OnTriggerStay(Collider col)
@@ -25,6 +34,7 @@ public class Multiplier_Spawn : MonoBehaviour {
         {
             transform.position = transform.position + new Vector3(Random.Range(-3, 3) + (m_DirectionToPlayer.x * 2), 0, Random.Range(-3, 3) + (m_DirectionToPlayer.z * 2));
             Debug.Log("hello");
+            ChangeRotation();
         }
     }
 
@@ -32,28 +42,38 @@ public class Multiplier_Spawn : MonoBehaviour {
     {
 
 
-        if(Mathf.Abs(m_PreviousPos.x - transform.position.x) > Mathf.Abs(m_PreviousPos.y - transform.position.y))
+        if (Mathf.Abs(m_PreviousPos.x - transform.position.x) > Mathf.Abs(m_PreviousPos.z - transform.position.z))
         {
-            if(m_PreviousPos.x - transform.position.x > 0)
+            if (m_PreviousPos.x - transform.position.x > 0)
             {
-
+                transform.eulerAngles = new Vector3(0, 270, 0);
             }
             else
             {
-
+                transform.eulerAngles = new Vector3(0, 90, 0);
             }
         }
         else
         {
-            if (m_PreviousPos.y - transform.position.y > 0)
+            if (m_PreviousPos.z - transform.position.z > 0)
             {
-
+                transform.eulerAngles = new Vector3(0, 180, 0);
             }
             else
             {
-
+                transform.eulerAngles = new Vector3(0, 0, 0);
             }
+
         }
-        
+    }
+
+    IEnumerator EnableColliderAndAnim()
+    {
+        yield return new WaitForSeconds(0.1f);
+        m_An.SetBool(0, true);
+        yield return new WaitForSeconds(0.5f);
+        m_CanBeTaken = true;
+
+        yield return null;
     }
 }
---
+++ Assets/Scripts/Multiplier/Multiplier_Value.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Multiplier_Value : MonoBehaviour {
+
+    [SerializeField]
+    float m_MultiplierValue;
+
+
+    public void AddMultiplierValueToScore()
+    {
+        GameManager_Score.Instance.IncreaseMultiplier(m_MultiplierValue);
+    }
+
+	}
